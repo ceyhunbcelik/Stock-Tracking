dfbb384b9b6f9d8d3cf01cb676b1bd03a1225fc2
ceyhunbcelik <ceyhunbcelik@icloud.com>
2021-01-10 23:00:54 +0300

    db first setup

--- Stock-Tracking/Program.cs
+++ Stock-Tracking/Program.cs
@@ -16,7 +16,7 @@ namespace Stock_Tracking
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Dashboard());
         }
     }
 }
